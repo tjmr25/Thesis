4539ed4a694f9c3665c89e3a89b2f3edb73fe018
tm117504 <tm117504@fh-muenster.de>
2025-04-10 13:13:45 +0200

    initial commit, added header, content and main. Added dummy text and a background picture

---
+++ thesis/src/app/content/content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-content',
+  imports: [MatCardModule],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.scss'
+})
+export class ContentComponent {
+
+}
